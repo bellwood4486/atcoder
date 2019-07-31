7df925d2489edd02c5b93fc61a63943efefcb1ed
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2019-07-31 22:37:48 +0900

    Add an answer ABC135_C

--- abc135/c/main.go
+++ abc135/c/main.go
@@ -18,27 +18,6 @@ func nextInt() int {
 	return i
 }
 
-func solve(n int, as []int, bs []int) int {
-	sum := 0
-	for i := len(bs) - 1; i >= 0; i-- {
-		if as[i+1] >= bs[i] {
-			sum += bs[i]
-			as[i+1], bs[i] = as[i+1] - bs[i], 0
-		} else {
-			sum += as[i+1]
-			as[i+1], bs[i] = 0, bs[i] - as[i+1]
-			if as[i] >= bs[i] {
-				sum += bs[i]
-				as[i] = as[i] - bs[i]
-			} else {
-				sum += as[i]
-				as[i] = 0
-			}
-		}
-	}
-	return sum
-}
-
 func main() {
 	sc.Split(bufio.ScanWords)
 	n := nextInt()
@@ -52,4 +31,51 @@ func main() {
 	}
 
 	fmt.Println(solve(n, as, bs))
+	//fmt.Println(answer(n, as, bs))
+}
+
+func answer(N int, A []int, B []int) int {
+	c := 0
+	for i := range B {
+		m := min(A[i], B[i])
+		A[i] -= m
+		B[i] -= m
+		c += m
+
+		m = min(A[i+1], B[i])
+		A[i+1] -= m
+		B[i] -= m
+		c += m
+	}
+
+	return c
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func solve(n int, as []int, bs []int) int {
+	sum := 0
+	for i := len(bs) - 1; i >= 0; i-- {
+		if as[i+1] >= bs[i] {
+			sum += bs[i]
+			as[i+1], bs[i] = as[i+1]-bs[i], 0
+		} else {
+			sum += as[i+1]
+			as[i+1], bs[i] = 0, bs[i]-as[i+1]
+			if as[i] >= bs[i] {
+				sum += bs[i]
+				as[i] = as[i] - bs[i]
+			} else {
+				sum += as[i]
+				as[i] = 0
+			}
+		}
+	}
+	return sum
 }
